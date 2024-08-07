3bab3b18d11ee4d40a49ad99e92921a4152189a6
FajarSant <4jar100so@gmail.com>
2024-08-08 00:08:25 +0700

    update

--- src/app/Postingan/[id]/page.tsx
+++ src/app/Postingan/[id]/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import {
-  FaArrowLeft,
   FaBuilding,
   FaEnvelope,
   FaPhone,
@@ -65,11 +64,14 @@ const PostinganDetail = () => {
   const handleShare = async () => {
     try {
       if (navigator.share) {
+        // Use Web Share API if supported
         await navigator.share({
-          title: "Lowongan Pekerjaan",
+          title: `Lowongan Pekerjaan di ${jobs?.namaPT}`,
+          text: jobs?.deskripsi || "",
           url: shareUrl,
         });
       } else {
+        // Fallback for browsers that do not support Web Share API
         const text = `Cek lowongan pekerjaan di ${jobs?.namaPT}. ${jobs?.openrekrutmen.join(', ')}\n${shareUrl}`;
         await navigator.clipboard.writeText(text);
         alert("Link telah disalin ke clipboard!");
@@ -85,113 +87,113 @@ const PostinganDetail = () => {
 
   return (
     <>
-    <Head>
+      <Head>
         <title>Lowongan Pekerjaan di {jobs.namaPT}</title>
         <meta property="og:title" content={`Lowongan Pekerjaan di ${jobs.namaPT}`} />
         <meta property="og:description" content={jobs.deskripsi} />
-        <meta property="og:image" content={jobs.gambar || defaultImage.src} />
+        <meta property="og:image" content={jobs.gambar ? jobs.gambar : defaultImage.src} />
         <meta property="og:url" content={shareUrl} />
         <meta property="og:type" content="website" />
       </Head>
-    <section className="text-gray-700 body-font overflow-hidden bg-white">
-      <div className="container px-5 py-24 mx-auto">
-        <Topbar />
-        <div className="lg:w-4/5 mx-auto flex flex-wrap mt-4 shadow-xl mb-5">
-          <Image
-            src={jobs.gambar || defaultImage}
-            alt={jobs.namaPT}
-            width={480}
-            height={240}
-            className="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200"
-          />
-          <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0 p-2">
-            <h2 className="badge badge-primary badge-outline">
-              {jobs.openrekrutmen}
-            </h2>
-            <h1 className="text-gray-900 text-3xl text-center title-font font-medium mb-4 mt-4">
-              {jobs.namaPT}
-            </h1>
-            <div className=" border-b-2 mb-2"></div>
-            <p className="leading-relaxed text-justify mb-4">
-              {jobs.deskripsi}
-            </p>
-            <div className=" border-b-2 mb-2"></div>
-            <div className="flex font-bold items-center mb-4">
-              <FiFileText className="mr-2" />
-              <span className="text-gray-600">
-                Berkas Yang Diperlukan : {jobs.berkas}
-              </span>
-            </div>
-            <div className=" border-b-2 mb-2"></div>
-            <div className="space-y-4 mb-4">
-              <div className="flex items-center">
-                <FaBuilding className="mr-2" />
+      <section className="text-gray-700 body-font overflow-hidden bg-white">
+        <div className="container px-5 py-24 mx-auto">
+          <Topbar />
+          <div className="lg:w-4/5 mx-auto flex flex-wrap mt-4 shadow-xl mb-5">
+            <Image
+              src={jobs.gambar || defaultImage}
+              alt={jobs.namaPT}
+              width={480}
+              height={240}
+              className="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200"
+            />
+            <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0 p-2">
+              <h2 className="badge badge-primary badge-outline">
+                {jobs.openrekrutmen}
+              </h2>
+              <h1 className="text-gray-900 text-3xl text-center title-font font-medium mb-4 mt-4">
+                {jobs.namaPT}
+              </h1>
+              <div className=" border-b-2 mb-2"></div>
+              <p className="leading-relaxed text-justify mb-4">
+                {jobs.deskripsi}
+              </p>
+              <div className=" border-b-2 mb-2"></div>
+              <div className="flex font-bold items-center mb-4">
+                <FiFileText className="mr-2" />
                 <span className="text-gray-600">
-                  Nama Perusahaan: {jobs.namaPT}
+                  Berkas Yang Diperlukan : {jobs.berkas}
                 </span>
               </div>
-              {jobs.alamat && (
+              <div className=" border-b-2 mb-2"></div>
+              <div className="space-y-4 mb-4">
                 <div className="flex items-center">
-                  <TbMapSearch className="mr-2" />
-                  <span className="text-gray-600">Alamat: {jobs.alamat}</span>
-                </div>
-              )}
-              {jobs.email && (
-                <div className="flex items-center">
-                  <FaEnvelope className="mr-2" />
-                  <span className="text-gray-600">Email: {jobs.email}</span>
-                </div>
-              )}
-              {jobs.nomorTelepon && (
-                <div className="flex items-center">
-                  <FaPhone className="mr-2" />
+                  <FaBuilding className="mr-2" />
                   <span className="text-gray-600">
-                    Nomor Telepon: {jobs.nomorTelepon}
+                    Nama Perusahaan: {jobs.namaPT}
                   </span>
                 </div>
+                {jobs.alamat && (
+                  <div className="flex items-center">
+                    <TbMapSearch className="mr-2" />
+                    <span className="text-gray-600">Alamat: {jobs.alamat}</span>
+                  </div>
+                )}
+                {jobs.email && (
+                  <div className="flex items-center">
+                    <FaEnvelope className="mr-2" />
+                    <span className="text-gray-600">Email: {jobs.email}</span>
+                  </div>
+                )}
+                {jobs.nomorTelepon && (
+                  <div className="flex items-center">
+                    <FaPhone className="mr-2" />
+                    <span className="text-gray-600">
+                      Nomor Telepon: {jobs.nomorTelepon}
+                    </span>
+                  </div>
+                )}
+              </div>
+              {jobs.persyaratan && (
+                <>
+                  <h2 className="text-gray-900 text-2xl title-font font-medium mb-4">
+                    Persyaratan
+                  </h2>
+                  <ul className="leading-relaxed mb-4 list-disc pl-6">
+                    {jobs.persyaratan.map((item, index) => (
+                      <li key={index} className="text-gray-600">
+                        {item}
+                      </li>
+                    ))}
+                  </ul>
+                </>
               )}
-            </div>
-            {jobs.persyaratan && (
-              <>
-                <h2 className="text-gray-900 text-2xl title-font font-medium mb-4">
-                  Persyaratan
-                </h2>
-                <ul className="leading-relaxed mb-4 list-disc pl-6">
-                  {jobs.persyaratan.map((item, index) => (
-                    <li key={index} className="text-gray-600">
-                      {item}
-                    </li>
-                  ))}
-                </ul>
-              </>
-            )}
-            <div className="flex justify-center space-x-4 mt-6">
-              <Button
-                className="text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-base md:text-lg"
-                onClick={handleApply}
-              >
-                Daftar
-              </Button>
-              <Button
-                className="text-white bg-blue-600 border-0 py-2 px-6 focus:outline-none hover:bg-blue-700 rounded text-base md:text-lg"
-                onClick={handleSave}
-              >
-                Simpan
-              </Button>
-             
-              <Button
-                className="flex items-center text-white bg-blue-500 border-0 py-2 px-4 focus:outline-none hover:bg-blue-600 rounded"
-                onClick={handleShare}
-              >
-                <FaShareAlt className="mr-2" />
-                Bagikan
-              </Button>
+              <div className="flex justify-center space-x-4 mt-6">
+                <Button
+                  className="text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-base md:text-lg"
+                  onClick={handleApply}
+                >
+                  Daftar
+                </Button>
+                <Button
+                  className="text-white bg-blue-600 border-0 py-2 px-6 focus:outline-none hover:bg-blue-700 rounded text-base md:text-lg"
+                  onClick={handleSave}
+                >
+                  Simpan
+                </Button>
+               
+                <Button
+                  className="flex items-center text-white bg-blue-500 border-0 py-2 px-4 focus:outline-none hover:bg-blue-600 rounded"
+                  onClick={handleShare}
+                >
+                  <FaShareAlt className="mr-2" />
+                  Bagikan
+                </Button>
   
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
     </>
   );
 };
